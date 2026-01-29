64a820d3a4ab35ca6d367b14d53c2df46f2cbfce
Ashish Kumar <soorveer31@gmail.com>
2026-01-29 21:21:55 +0530

    Corrected linear search

--- Linear_Search.c
+++ Linear_Search.c
@@ -1,32 +1,41 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int Searching(int arr[],int size,int find){
-  int index;
+int Searching(int arr[], int size, int find)
+{
+  int value = 0;
   for (int i = 0; i < size; i++)
   {
     if (find == arr[i])
     {
-      index = arr[i];
-      return index;
-    }else
-    {
-      return -1;
+      value = arr[i];
+      break;
     }
   }
-  
+  return value;
 }
-int main(){
+
+int main()
+{
   int arr[5];
-  int size = sizeof(arr)/sizeof(arr[0]);
+  int size = sizeof(arr) / sizeof(arr[0]);
   int find;
   for (int i = 0; i < size; i++)
   {
     printf("Enter the value: ");
-    scanf("%d",&arr[i]);
+    scanf("%d", &arr[i]);
   }
-  for (int i = 0; i < size; i++)
+  printf("Enter the value to find: ");
+  scanf("%d", &find);
+
+  int search = Searching(arr, size, find);
+  
+  if (search)
   {
-    pritf
-  int found = Searching(arr,size,find)
+    printf("Number found");
+  }
+  else
+  {
+    printf("Number not found");
+  }
   return 0;
 }
